043ef1edc1cdbcaca1aadcd7ea402294385ed303
bexqth <nicollsprite@gmail.com>
2024-03-29 21:30:24 +0100

    default, hit, focused transitions in tree

--- scripts/Tree.cs
+++ scripts/Tree.cs
@@ -3,7 +3,6 @@ using System;
 
 public partial class Tree : ResourceNode
 {
-	World world;
 	private ResourceItem resourceItem;
 	private PackedScene resourceItemScene;
 	private int resourceItemCount;
@@ -11,6 +10,11 @@ public partial class Tree : ResourceNode
 	private bool dealtDamage;
 	private int recievedDamage;
 	private Player player;
+	private Timer spriteChangingTimer;
+	private Sprite2D defaultSprite;
+	private Sprite2D focusedSprite;
+	private Sprite2D hitSprite;
+	private bool mouseEntered;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -18,15 +22,18 @@ public partial class Tree : ResourceNode
 		this.resourceItem = (Log)resourceItemScene.Instantiate();
 		this.setresourceItem(this.resourceItem);
 		this.setresourceItemScene(this.resourceItemScene);
-		world = (World)GetNode("/root/World");
+		this.defaultSprite = GetNode<Sprite2D>("Sprite2D_default");
+		this.focusedSprite = GetNode<Sprite2D>("Sprite2D_focused");
+		this.hitSprite = GetNode<Sprite2D>("Sprite2D_hit");
+		spriteChangingTimer = GetNode<Timer>("sprite_changing_Timer");
+		this.showDefaultSprite();
+		this.mouseEntered = false;
 		//GD.Print("tree - " + world);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//GD.PrintT("using axe " + world.getPlayerUsingAxe());
-		//GD.Print(world.getPlayerUsingAxe());
 		base._Process(delta);
 		dealDamage();
 	}
@@ -36,6 +43,8 @@ public partial class Tree : ResourceNode
 			if(player.axeAnimationEnd()) {
 				reduceLife(recievedDamage);
 				GD.Print("life is " + this.getLife());
+				this.showHitSprite();
+				spriteChangingTimer.Start();
 				dealtDamage = false;
 			}
 		}
@@ -66,13 +75,51 @@ public partial class Tree : ResourceNode
 	
 	private void _on_area_2d_range_body_exited(Node2D body)
 	{
-		if(body is Player player) {
+		if(body is Player) {
 			GD.Print("Player out of the range");
 			canBeHit = false;
 			this.player = null;
 		}
 	}
 
+
+	public void showDefaultSprite() {
+		this.defaultSprite.Show();
+		this.focusedSprite.Hide();
+		this.hitSprite.Hide();
+	}
+
+	public void showFocusedSprite() {
+		this.defaultSprite.Hide();
+		this.focusedSprite.Show();
+		this.hitSprite.Hide();
+	}
+	public void showHitSprite() {
+		this.defaultSprite.Hide();
+		this.focusedSprite.Hide();
+		this.hitSprite.Show();
+	}
+
+	private void _on_sprite_changing_timer_timeout()
+	{
+		if(mouseEntered) {
+			this.showFocusedSprite();
+		} else {
+			this.showDefaultSprite();
+		}
+	}
+
+	private void _on_area_2d_mouse_entered()
+	{
+		this.mouseEntered = true;
+		this.showFocusedSprite();
+	}
+
+	private void _on_area_2d_mouse_exited()
+	{
+		this.mouseEntered = false;
+		this.showDefaultSprite();
+	}
 
 }
 
